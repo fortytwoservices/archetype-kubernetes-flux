c5ad08f226c946c212f71231dc8b3bf02554c906
Roberth Strand <roberth.strand@amesto.no>
2023-03-01 09:23:24 +0100

    feat: initial azure networking
    Signed-off-by: Roberth Strand <roberth.strand@amesto.no>

--- main.tf
+++ main.tf
@@ -3,6 +3,21 @@ resource "azurerm_resource_group" "cluster" {
   location = var.location
 }
 
+resource "azurerm_virtual_network" "cluster" {
+  name                = format("vnet-%s", var.environment_name)
+  resource_group_name = azurerm_resource_group.cluster.name
+  location            = azurerm_resource_group.cluster.location
+  address_space       = var.azure_network.vnet_cidr
+  tags                = var.tags
+}
+
+resource "azurerm_subnet" "aks" {
+  name                 = "snet-aks"
+  resource_group_name  = azurerm_resource_group.cluster.name
+  virtual_network_name = azurerm_virtual_network.cluster.name
+  address_prefixes     = var.azure_network.subnet_cidr
+}
+
 module "aks" {
   source              = "amestofortytwo/aks/azurerm"
   version             = "1.1.0"
@@ -10,6 +25,11 @@ module "aks" {
   resource_group_name = azurerm_resource_group.cluster.name
   location            = azurerm_resource_group.cluster.location
   tags                = var.tags
+
+  network_profile = {
+    network_plugin = "azure"
+    vnet_subnet_id = azurerm_subnet.aks.id
+  }
 
   default_node_pool = {
     vm_size    = var.node_pools.default.vm_size
